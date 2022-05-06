8eea0635399aaa3e89295c409e6888d87fc55a9c
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2022-05-06 17:34:59 -0300

    angular + springboot

--- spring-boot-server/src/main/java/com/bezkoder/spring/data/mongodb/controller/TutorialController.java
+++ spring-boot-server/src/main/java/com/bezkoder/spring/data/mongodb/controller/TutorialController.java
@@ -20,8 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bezkoder.spring.data.mongodb.model.Tutorial;
 import com.bezkoder.spring.data.mongodb.repository.TutorialRepository;
-
 @CrossOrigin(origins = "http://angular.harness-demo.site")
+//@CrossOrigin(origins = {"http://34.122.165.247"})
+//@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
